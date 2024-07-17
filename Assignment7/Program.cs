23e73ddf257a0478dc589f383b27d6f373eaa4b4
Banday Chandini <chandinibanday@gmail.com>
2024-07-17 23:33:16 +0530

    Assignment7

---
+++ Assignment7/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Assignment5
+{
+    // Accept 10 numbers and sort the data in ascending order and display it.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.WriteLine($"Elements are: {i + 1}");
+                int lists= int.Parse(Console.ReadLine());
+                list.Add(lists);
+            }
+            list.Sort();
+            foreach (var num in list)
+            {
+                Console.Write(num+" ");
+            }
+        }
+    }
+}
